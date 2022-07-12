0a0158e536d22c1b7512e6786fcef13d48dd7a93
Mikhail <tixonkov29@gmail.com>
2022-07-12 12:39:05 +1000

    щл

--- TS/Class/Human.js
+++ TS/Class/Human.js
@@ -1,19 +1,14 @@
 //Создание класса с хранящимися в нем параметрами
-class Human {
-    Name:string;
-    FirstName:String;
-    Age:number;
-    sex:boolean;
-    constructor (Name:string, FirstName:string, Age:number, sex:boolean)
-    {
-        this.Age=Age;
-        this.Name=Name;
-        this.FirstName=FirstName;
-        this.sex=sex;
+var Human = /** @class */ (function () {
+    function Human(Name, FirstName, Age, sex) {
+        this.Age = Age;
+        this.Name = Name;
+        this.FirstName = FirstName;
+        this.sex = sex;
     }
-}
+    return Human;
+}());
 //создание переменной с типом данных "Human"
 //let user:Human= new Human("Ben","Ten",15,true);
 //Вывод строки 
 //console.log(user);
-
